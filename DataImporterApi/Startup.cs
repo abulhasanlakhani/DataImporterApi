daf4188eef65e09109715b1122e639232abb2313
Abul Hasan Lakhani <abulhasanlakhani@gmail.com>
2019-01-05 10:54:00 +1300

    #3 DNCI-3: Add and configure CORS support

--- DataImporterApi/Startup.cs
+++ DataImporterApi/Startup.cs
@@ -18,6 +18,7 @@ namespace DataImporterApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => options.AddPolicy("MyPolicy", policy => policy.AllowAnyOrigin()));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -35,6 +36,7 @@ namespace DataImporterApi
             }
 
             app.UseHttpsRedirection();
+            app.UseCors("MyPolicy");
             app.UseMvc();
         }
     }
