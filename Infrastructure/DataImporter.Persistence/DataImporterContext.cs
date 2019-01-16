3e19627a172a2d6b4d1c9135ef817885f0e683bb
Abul Hasan Lakhani <abulhasanlakhani@gmail.com>
2019-01-16 22:10:55 +1300

    #17 EFCI-2: Added EF Core and basic db context with one db set

---
+++ Infrastructure/DataImporter.Persistence/DataImporterContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataImporter.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataImporter.Persistence
+{
+    public class DataImporterContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
