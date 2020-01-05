f7e950aaa8f33b248af8a40655cdb6f3ce37a3cb
Abul Hasan Lakhani <abul.lakhani@kiwiplan.co.nz>
2020-01-05 21:18:16 +1300

    Some tidy up plus added exception handling

--- Presentation/DataImporterApi/Startup.cs
+++ Presentation/DataImporterApi/Startup.cs
@@ -2,7 +2,6 @@
 using DataImporter.Business.Interfaces;
 using DataImporter.Business.Services;
 using DataImporter.Persistence;
-using DataImporterApi.Filters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
@@ -27,8 +26,6 @@ namespace DataImporterApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddCors(options => options.AddPolicy("MyPolicy", policy => policy.AllowAnyOrigin()));
-
             services.AddControllers(setupAction =>
             {
                 setupAction.ReturnHttpNotAcceptable = true;
@@ -47,7 +44,6 @@ namespace DataImporterApi
                 setupAction.SerializerSettings.ContractResolver =
                    new CamelCasePropertyNamesContractResolver();
             })
-            .AddXmlDataContractSerializerFormatters()
             .ConfigureApiBehaviorOptions(setupAction =>
             {
                 setupAction.InvalidModelStateResponseFactory = context =>
@@ -69,12 +65,6 @@ namespace DataImporterApi
                     };
                 };
             });
-
-            services.AddMvc(options => 
-                {
-                    options.Filters.Add<JsonExceptionFilter>();
-                    options.Filters.Add<RequireHttpsOrCloseAttribute>();
-                });
 
             services.AddSwaggerDocument();
 
@@ -98,12 +88,19 @@ namespace DataImporterApi
             }
             else
             {
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("An unexpected fault happened. Try again later.");
+                    });
+                });
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
-            //app.UseCors("MyPolicy");
-            
             app.UseHttpsRedirection();
 
             app.UseOpenApi();
